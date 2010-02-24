ff3ffee64190d04f95c43f2ca7b54322e1690634
brunomlopes <brunomlopes@gmail.com>
2010-02-24 16:58:41 +0000

    Web changed to .net 3.5. Using spark compatible with mvc2

--- source/Web/Global.asax.cs
+++ source/Web/Global.asax.cs
@@ -1,17 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
+using Autofac;
+using Autofac.Integration.Web;
+using Autofac.Integration.Web.Mvc;
+using Spark;
+using Spark.Web.Mvc;
 
 namespace Web
 {
     // Note: For instructions on enabling IIS6 or IIS7 classic mode, 
     // visit http://go.microsoft.com/?LinkId=9394801
 
-    public class MvcApplication : System.Web.HttpApplication
+    public class MvcApplication : System.Web.HttpApplication, IContainerProviderAccessor
     {
+        static IContainerProvider _containerProvider;
+
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
@@ -21,14 +29,41 @@ namespace Web
                 "{controller}/{action}/{id}",                           // URL with parameters
                 new { controller = "Home", action = "Index", id = "" }  // Parameter defaults
             );
-
         }
 
         protected void Application_Start()
         {
+            SetupSpark();
+
+            SetupAutofac();
+
             AreaRegistration.RegisterAllAreas();
 
             RegisterRoutes(RouteTable.Routes);
+        }
+
+        private void SetupSpark()
+        {
+            var settings = new SparkSettings()
+                .SetDebug(true)
+                .AddAssembly("Web");
+            ViewEngines.Engines.Add(new SparkViewFactory(settings));
+        }
+
+        private void SetupAutofac()
+        {
+            var builder = new ContainerBuilder();
+
+            builder.RegisterControllers(Assembly.GetExecutingAssembly());
+
+            _containerProvider = new ContainerProvider(builder.Build());
+
+            ControllerBuilder.Current.SetControllerFactory(new AutofacControllerFactory(ContainerProvider));
+        }
+
+        public IContainerProvider ContainerProvider
+        {
+            get { return _containerProvider; }
         }
     }
 }
